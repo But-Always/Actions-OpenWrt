50531ee30445799a005925898fbe881d418beb7a
王衍辰 <1336083296@qq.com>
2019-12-13 17:16:59 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
 
+mkdir -r package/openwrt-packages
+
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/lean
 git clone https://github.com/Ameykyl/luci-app-koolproxyR package package/lean
 git clone https://github.com/rosywrt/luci-theme-rosy package package/lean
