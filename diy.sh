2d2ba4919830a7b755151a13d37daaaa4cee6f52
王衍辰 <1336083296@qq.com>
2019-12-15 10:15:27 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,5 +11,5 @@ sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_gene
 mkdir -p package/openwrt-packages
 
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/openwrt-packages
-git clone https://github.com/Ameykyl/luci-app-koolproxyR package package/openwrt-packages
-git clone https://github.com/rosywrt/luci-theme-rosy package package/openwrt-packages
+git clone https://github.com/Ameykyl/luci-app-koolproxyR package/openwrt-packages
+git clone https://github.com/rosywrt/luci-theme-rosy package/openwrt-packages
