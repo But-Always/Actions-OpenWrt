c2d6b65dc6908f00007e3159bb51c80323a2d66c
王衍辰 <1336083296@qq.com>
2020-04-24 09:50:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,6 +10,9 @@ sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_gene
 
 mkdir -p package/openwrt-packages
 
-git clone https://github.com/brokeld/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
+git clone https://github.com/But-Always/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
 
-git clone https://github.com/vernesong/OpenClash.git package/openwrt-packages/luci-app-openclash
+# git clone https://github.com/vernesong/OpenClash.git package/openwrt-packages/luci-app-openclash
+
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages/
+
