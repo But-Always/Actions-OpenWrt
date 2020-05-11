1955c2d71596587f96f8a0dafe8a4df4e08fdd82
wangyanchen <1336083296@qq.com>
2020-05-11 15:28:04 +0800

    新版编译测试

---
+++ diy-part2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i "" 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
+
+
+
+
