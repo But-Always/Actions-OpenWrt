8e9c249dbfae61d8e2496500dbf898d9ad59708c
wangyanchen <1336083296@qq.com>
2020-05-11 15:52:32 +0800

    问题修改

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,18 +6,18 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i "" 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
 
-sed -i "" 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 mkdir -p package/openwrt-packages
 
 git clone https://github.com/But-Always/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
 
 # Add a feed source
-sed -i "" '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i "" '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i "" '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # git clone https://github.com/vernesong/OpenClash.git package/openwrt-packages/luci-app-openclash
 
